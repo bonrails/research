439518462f25fbb41855fe1470580c4b9626024b
BhavaniG1 <BhavaniG1@verifone.com>
2016-04-20 15:14:24 -0700

    code test 2

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,6 +1,7 @@
 class CampaignsController < ApplicationController
 
   def ordered_campaigns
-    render :text => '<b>Not sure what to do :-)</b>'
+    @list=Campaign.includes(campaign_quotas: [:campaign_qualifications]).where('campaign_qualifications.question_id IN (?)', [42,43]).references(campaign_quotas: :campaign_qualifications).order(:length_of_interview, cpi: :desc)
+    render :index
   end
 end
